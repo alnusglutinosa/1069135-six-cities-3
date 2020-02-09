3b1470c367f179aa0f6a94e1c04681c4202bd4d2
alnusglutinosa <olga.app2@gmail.com>
2020-02-09 11:35:46 +0200

    module2-task1 валидируй, как никогда не валидировал

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,14 +3,16 @@ import Main from "../main/main.jsx";
 import PropTypes from "prop-types";
 
 const App = (props) => {
-  const {rentalCount} = props;
+  const {rentalCount, topPlacesName} = props;
   return <Main
     rentalCount={rentalCount}
+    topPlacesName={topPlacesName}
   />;
 };
 
 App.propTypes = {
-  rentalCount: PropTypes.number.isRequired
+  rentalCount: PropTypes.number.isRequired,
+  topPlacesName: PropTypes.arrayOf(PropTypes.string.isRequired)
 };
 
 export default App;
