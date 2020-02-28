62033026e1e87d958e8c24f7bf130b974731c74b
alnusglutinosa <olga.app2@gmail.com>
2020-02-28 18:16:31 +0200

    module3-task2 таинственные подробности

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import Main from "../main/main.jsx";
+import Property from "../property/property.jsx";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 
-const onMainButtonClick = () => {};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-const App = (props) => {
-  const {rentalCount, offers} = props;
-  return <Main
-    rentalCount={rentalCount}
-    offers={offers}
-    onMainButtonClick={onMainButtonClick}
-  />;
-};
+  _renderApp() {
+    const onMainButtonClick = () => {};
+    const {rentalCount, offers} = this.props;
+
+    if (offers) {
+      return (
+        <Main
+          rentalCount={rentalCount}
+          offers={offers}
+          onMainButtonClick={onMainButtonClick}
+        />
+      );
+    }
+
+    return null;
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+          </Route>
+          <Route exact path="/offer">
+            <Property />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   rentalCount: PropTypes.number.isRequired,
