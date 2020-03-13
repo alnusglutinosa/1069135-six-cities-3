e17600d4bde0e08109600a2bad85eee7dc56fbc9
alnusglutinosa <olga.app2@gmail.com>
2020-03-13 11:34:24 +0200

    стейт под полным контролем

--- src/index.js
+++ src/index.js
@@ -2,19 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 import offers from "./mocks/offers";
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer.js';
 
 const Settings = {
-  RENTAL_COUNT: 312,
   CITY: [52.38333, 4.9],
   ZOOM: 12,
 };
 
+const rootElement = document.getElementById(`root`);
+const rentalCount = offerMocks.length;
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f);
+
+
+
 ReactDOM.render(
-    <App
-      rentalCount={Settings.RENTAL_COUNT}
-      city={Settings.CITY}
-      zoom={Settings.ZOOM}
-      offers={offers}
-    />,
-    document.querySelector(`#root`)
-);
+  <Provider store={store}>
+      <App
+        rentalCount={rentalCount}
+        city={Settings.CITY}
+        zoom={Settings.ZOOM}
+        offers={offers}
+      />
+  </Provider>,rootElement);
